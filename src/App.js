8ab23d92b8803cc600057cf600e5670dff5b6912
Emily Byrne <embyrne13@gmail.com>
2022-07-13 20:48:22 -0400

    password hw

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
-import Validator from "./components/Validator"
-
+import { useState } from 'react'
+import Validator from './components/Validator'
 const App = () => {
-
+  const [web, setWeb] = useState({
+    username: '',
+    password: '',
+    passwordConfirm: ''
+  })
+  const [finalWeb, setFinalWeb] = useState({
+    username: '',
+    password: '',
+    passwordConfirm: '',
+    valid: false
+  })
+  const [validity, setValidity] = useState('')
+  const handleChange = (event) => {
+    let newWeb = { ...web, [event.target.id]: event.target.value }
+    setWeb(newWeb)
+  }
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    if (web.password === web.passwordConfirm) {
+      console.log('Passwords match')
+      setFinalWeb(web)
+      setValidity('valid')
+    } else {
+      console.log('Passwords do not match')
+      setValidity('invalid')
+    }
+  }
   return (
-    <Validator />
+    <Validator
+      validity={validity}
+      handleChange={handleChange}
+      handleSubmit={handleSubmit}
+    />
   )
 }
 
--- src/components/Validator.jsx
+++ src/components/Validator.jsx
@@ -1,26 +1,41 @@
 import '../styles/Validator.css'
 
-const Validator = () => {
-  
+const Validator = (props) => {
   return (
     <div className="form">
       <h1>Sign Up</h1>
-      <form>
-        <input type="text" placeholder="Username" id="username" />
+      <form onSubmit={props.handleSubmit}>
+        <input
+          type="text"
+          placeholder="Username"
+          id="username"
+          onChange={props.handleChange}
+        />
         <label htmlFor="username">Username</label>
 
-        <input type="password" placeholder="Password" id="password" />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          onChange={props.handleChange}
+        />
         <label htmlFor="password">Password</label>
 
         <input
           type="password"
           placeholder="Confirm password"
           id="passwordConfirm"
+          onChange={props.handleChange}
         />
         <label htmlFor="passwordConfirm">Confirm password</label>
 
-        <button type="submit">Sign Up</button>
-        <p>Passwords must match.</p>
+        <button className="submit" type="submit">
+          Sign Up
+        </button>
+        <button className="reset" type="reset">
+          Reset
+        </button>
+        <p className={props.validity}>Passwords must match.</p>
       </form>
     </div>
   )
